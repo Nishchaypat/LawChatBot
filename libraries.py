fa7c8672a4d9cf2dedbd7efeee4490afa5637b33
Manish Kolla <maneeshkolla@gmail.com>
2025-02-09 11:01:14 -0500

    lib changed

--- libraries.py
+++ libraries.py
@@ -5,6 +5,7 @@ import torch
 from transformers import AutoTokenizer, AutoModel
 from qdrant_client import QdrantClient
 from qdrant_client.http.models import VectorParams, Distance, Filter, SearchRequest
+from sklearn.metrics.pairwise import cosine_similarity
 #from sentence_transformers import SentenceTransformerimport
 import faiss
 import os
