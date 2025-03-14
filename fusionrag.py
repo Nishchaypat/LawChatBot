c449f50bd69a25aafc83d11f1f54c62193156dfe
Manish Kolla <maneeshkolla@gmail.com>
2025-03-14 13:13:18 -0400

    updated fusion rag with final legalchatbot function which calls all other functions

--- fusionrag.py
+++ fusionrag.py
@@ -15,6 +15,9 @@ voyage_sections_df = pd.read_parquet(r"New_Embeddings_2025\sections\embeddings_v
 voyage_pages_df = pd.read_parquet(r"New_Embeddings_2025\pages\embeddings_voyage_per_pages_semchunked.parquet")
 voyage_chapters_df = pd.read_parquet(r"New_Embeddings_2025\chapters\embeddings_voyage_per_chapter_semchunked.parquet")
 
+'''
+Retriving the Content based on the Indices retrived from the Retrival process
+'''
 def get_processed_content_by_index(index, source, model):
     """
     Retrieve the Processed_Content or chunk based on the specified index and source.
@@ -44,30 +47,10 @@ def get_processed_content_by_index(index, source, model):
             return voyage_chapters_df.loc[index, "chunk"] if index in voyage_chapters_df.index else None
         else:
             return "Invalid source specified."
-        
 
-base_path = "New_Embeddings_2025" 
-query = """If someone knowingly transports a person they know to be a terrorist on a vessel within the U.S., on waters under U.S. jurisdiction, or on a U.S. vessel on the high seas, what are the potential penalties? Also, how is 'terrorist' defined in this context?
-"""
-results= testing(base_path, query, forward_fn="", filter_fn= True)
-print(results)
 
-structured_data = {
-    "sections": {
-        "gemini": list(map(lambda x: get_processed_content_by_index(x, 'sections','gemini' ), results['top_indices']['sections']['gemini_top_indices'])),
-        "voyage": list(map(lambda x: get_processed_content_by_index(x, 'sections','voyage'), results['top_indices']['sections']['voyager_top_indices'])),
-    },
-    "chapters": {
-        "gemini": list(map(lambda x: get_processed_content_by_index(x, 'chapters', 'gemini'), results['top_indices']['chapters']['gemini_top_indices'])),
-        "voyage": list(map(lambda x: get_processed_content_by_index(x, 'chapters', 'voyage'), results['top_indices']['chapters']['voyager_top_indices'])),
-    },
-    "pages": {
-        "gemini": list(map(lambda x: get_processed_content_by_index(x, 'pages', 'gemini'), results['top_indices']['pages']['gemini_top_indices'])),
-        "voyage": list(map(lambda x: get_processed_content_by_index(x, 'pages', 'voyage'), results['top_indices']['pages']['voyager_top_indices'])),
-    }
-}
 
-print(structured_data)
+# print(structured_data)
 prompt= """You are a highly specialized legal expert and research assistant. Your expertise is strictly confined to legal principles, case law, statutes, and regulatory frameworks. You possess a deep understanding of legal terminology and can accurately interpret complex legal information.
 
 Your primary function is to provide precise and concise answers to legal questions based on the provided context. You will first retrieve relevant information from the provided legal documents and then synthesize a response that directly addresses the user's query.
@@ -90,6 +73,9 @@ The input data consists of structured information extracted from two distinct mo
 Answer the following legal question based on the provided content: {query_here}
 """
 
+'''
+Augmented generation with Prompt Engineering
+'''
 def generate_response(prompt,  query, data):
     model = genai.GenerativeModel('models/gemini-2.0-flash',
                               system_instruction=prompt.format(query_here=query, data_here=data),
@@ -100,8 +86,33 @@ def generate_response(prompt,  query, data):
     else:
         return "No response generated. Please check the input data or model configuration."
 
-print("#################################################################################################")
-print("Gemini Response:")
-gemini_response = generate_response(prompt, query, structured_data)
-print(gemini_response)
-print("#################################################################################################")
+
+'''
+Final Function to retrive the indices, text, and generate response
+'''
+def LegalChatBot(query):
+    base_path = "New_Embeddings_2025" 
+    results= testing(base_path, query, forward_fn="Advanced", filter_fn= True)
+    structured_data = {
+    "sections": {
+        "gemini": list(map(lambda x: get_processed_content_by_index(x, 'sections','gemini' ), results['top_indices']['sections']['gemini_top_indices'])),
+        "voyage": list(map(lambda x: get_processed_content_by_index(x, 'sections','voyage'), results['top_indices']['sections']['voyager_top_indices'])),
+    },
+    "chapters": {
+        "gemini": list(map(lambda x: get_processed_content_by_index(x, 'chapters', 'gemini'), results['top_indices']['chapters']['gemini_top_indices'])),
+        "voyage": list(map(lambda x: get_processed_content_by_index(x, 'chapters', 'voyage'), results['top_indices']['chapters']['voyager_top_indices'])),
+    },
+    "pages": {
+        "gemini": list(map(lambda x: get_processed_content_by_index(x, 'pages', 'gemini'), results['top_indices']['pages']['gemini_top_indices'])),
+        "voyage": list(map(lambda x: get_processed_content_by_index(x, 'pages', 'voyage'), results['top_indices']['pages']['voyager_top_indices'])),
+    }
+}
+    print("#################################################################################################")
+    print("Gemini Response:")
+    gemini_response = generate_response(prompt, query, structured_data)
+    print(gemini_response)
+    print("#################################################################################################")
+    #return gemini_response
+
+query = """Can you explain the rules regarding venue in federal criminal procedure?"""
+LegalChatBot(query)
